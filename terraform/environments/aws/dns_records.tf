0ae64e537df0358e72b0a209dbc715c0d1175517
Cristian Magherusan-Stanciu <cristi@leanercloud.com>
2026-02-25 23:07:09 +0100

    fix(terraform/aws): correct Fargate deployment config and frontend DNS refs
    - Use provider 5.x top-level attributes for ECS service deployment config
      instead of unsupported deployment_configuration block
    - Add enable_frontend guard to frontend_alias DNS record count condition
    - Use module.frontend[0] indexing for count-enabled frontend module

--- terraform/environments/aws/dns_records.tf
+++ terraform/environments/aws/dns_records.tf
@@ -1,14 +1,14 @@
 # DNS record for frontend (created here to avoid count issues with computed zone_id)
 resource "aws_route53_record" "frontend_alias" {
-  count = var.subdomain_zone_name != "" && length(var.frontend_domain_names) > 0 ? 1 : 0
+  count = var.enable_frontend && var.subdomain_zone_name != "" && length(var.frontend_domain_names) > 0 ? 1 : 0
 
   zone_id = local.subdomain_zone_id
   name    = var.frontend_domain_names[0]
   type    = "A"
 
   alias {
-    name                   = module.frontend.cloudfront_domain_name
-    zone_id                = module.frontend.cloudfront_hosted_zone_id
+    name                   = module.frontend[0].cloudfront_domain_name
+    zone_id                = module.frontend[0].cloudfront_hosted_zone_id
     evaluate_target_health = false
   }
 
--- terraform/modules/compute/aws/fargate/main.tf
+++ terraform/modules/compute/aws/fargate/main.tf
@@ -489,14 +489,12 @@ resource "aws_ecs_service" "main" {
 
   enable_execute_command = var.enable_execute_command
 
-  deployment_configuration {
-    maximum_percent         = 200
-    minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
 
-    deployment_circuit_breaker {
-      enable   = true
-      rollback = true
-    }
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
   }
 
   depends_on = [
