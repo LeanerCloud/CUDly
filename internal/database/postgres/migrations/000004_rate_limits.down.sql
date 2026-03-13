798d89e6cb6c3c8a99f807794268e1a2a7526010
Cristian Magherusan-Stanciu <cristi@leanercloud.com>
2026-03-13 20:56:14 +0100

    fix(migrations): add CASCADE to rate_limits drop for consistency
    Addresses: PR_review/internal_database_postgres_migrations_000004_rate_limits.down.sql.md > DROP_TABLE_rate_limits (REVIEWED) > [BUG] DROP TABLE IF EXISTS rate_limits does not include CASCADE

--- internal/database/postgres/migrations/000004_rate_limits.down.sql
+++ internal/database/postgres/migrations/000004_rate_limits.down.sql
@@ -1,4 +1,4 @@
 -- Drop rate_limits table
 DROP FUNCTION IF EXISTS cleanup_expired_rate_limits();
 DROP INDEX IF EXISTS idx_rate_limits_reset_time;
-DROP TABLE IF EXISTS rate_limits;
+DROP TABLE IF EXISTS rate_limits CASCADE;
