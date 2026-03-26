06cdf737eea680200bbbe6090c9fde427d6e6bd7
Cristian Magherusan-Stanciu <cristi@leanercloud.com>
2026-03-26 01:23:47 +0100

    fix(aws/ecr): add force_delete and fix module structure
    - Add force_delete = true to ECR repository to prevent
      RepositoryNotEmptyException during terraform destroy
    - Split module into standard structure (main.tf, variables.tf,
      outputs.tf, versions.tf) to satisfy tflint

--- terraform/modules/registry/aws/main.tf
+++ terraform/modules/registry/aws/main.tf
@@ -1,30 +1,8 @@
-variable "repository_name" {
-  description = "Name of the ECR repository"
-  type        = string
-}
-
-variable "keep_image_count" {
-  description = "Number of tagged images to keep"
-  type        = number
-  default     = 10
-}
-
-variable "untagged_expiry_days" {
-  description = "Days after which untagged images are deleted"
-  type        = number
-  default     = 7
-}
-
-variable "tags" {
-  description = "Tags to apply to all resources"
-  type        = map(string)
-  default     = {}
-}
-
 # ECR Repository
 resource "aws_ecr_repository" "main" {
   name                 = var.repository_name
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -96,20 +74,4 @@ resource "aws_ecr_repository_policy" "main" {
       }
     ]
   })
-}
-
-# Outputs
-output "repository_url" {
-  description = "URL of the ECR repository"
-  value       = aws_ecr_repository.main.repository_url
-}
-
-output "repository_arn" {
-  description = "ARN of the ECR repository"
-  value       = aws_ecr_repository.main.arn
-}
-
-output "repository_name" {
-  description = "Name of the ECR repository"
-  value       = aws_ecr_repository.main.name
 }
