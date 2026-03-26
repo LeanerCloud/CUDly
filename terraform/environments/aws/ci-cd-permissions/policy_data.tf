08c31057ef2a56bdd198ff70b3b8fda6109a95a1
Cristian Magherusan-Stanciu <cristi@leanercloud.com>
2026-03-26 01:03:47 +0100

    fix(cleanup): use force-delete for ECR, disable RDS protection, fix GCP VPC peering
    - Replace ECR batch-delete-image with delete-repository --force (simpler,
      handles images atomically without needing separate list/delete steps)
    - Add rds:CreateDBSnapshot to IAM data policy (state stores original
      skip_final_snapshot=false value, requires this permission on destroy)
    - Add step to disable RDS deletion_protection before terraform destroy
      (state stores the original true value, tfvars change doesn't override during destroy)
    - Remove Service Networking Connection from state and delete via gcloud before
      terraform destroy to avoid Cloud SQL VPC peering hold-up in GCP

---
+++ terraform/environments/aws/ci-cd-permissions/policy_data.tf
@@ -0,0 +1,157 @@
+resource "aws_iam_policy" "data" {
+  name        = "cudly-deploy-data"
+  description = "CUDly Terraform deploy: RDS, SecretsManager, IAM, S3, CloudTrail"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "IAMRolesAndPolicies"
+        Effect = "Allow"
+        Action = [
+          "iam:AddRoleToInstanceProfile",
+          "iam:AttachRolePolicy",
+          "iam:CreateInstanceProfile",
+          "iam:CreatePolicy",
+          "iam:CreatePolicyVersion",
+          "iam:CreateRole",
+          "iam:DeleteInstanceProfile",
+          "iam:DeletePolicy",
+          "iam:DeletePolicyVersion",
+          "iam:DeleteRole",
+          "iam:DeleteRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:GetInstanceProfile",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:GetRole",
+          "iam:GetRolePolicy",
+          "iam:ListAttachedRolePolicies",
+          "iam:ListInstanceProfilesForRole",
+          "iam:ListPolicyVersions",
+          "iam:ListRolePolicies",
+          "iam:ListRoleTags",
+          "iam:PassRole",
+          "iam:PutRolePolicy",
+          "iam:RemoveRoleFromInstanceProfile",
+          "iam:TagInstanceProfile",
+          "iam:TagPolicy",
+          "iam:TagRole",
+          "iam:UntagInstanceProfile",
+          "iam:UntagPolicy",
+          "iam:UntagRole",
+        ]
+        Resource = [
+          "arn:aws:iam::*:role/cudly-*",
+          "arn:aws:iam::*:policy/cudly-*",
+          "arn:aws:iam::*:instance-profile/cudly-*",
+        ]
+      },
+      {
+        Sid    = "IAMReadForPassRole"
+        Effect = "Allow"
+        Action = [
+          "iam:GetRole",
+          "iam:GetInstanceProfile",
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "IAMServiceLinkedRole"
+        Effect = "Allow"
+        Action = ["iam:CreateServiceLinkedRole"]
+        Resource = [
+          "arn:aws:iam::*:role/aws-service-role/rds.amazonaws.com/AWSServiceRoleForRDS",
+          "arn:aws:iam::*:role/aws-service-role/ecs.application-autoscaling.amazonaws.com/AWSServiceRoleForApplicationAutoScaling_ECSService",
+        ]
+        Condition = {
+          StringLike = {
+            "iam:AWSServiceName" = [
+              "rds.amazonaws.com",
+              "ecs.application-autoscaling.amazonaws.com",
+            ]
+          }
+        }
+      },
+      {
+        Sid    = "RDS"
+        Effect = "Allow"
+        Action = [
+          "rds:AddTagsToResource",
+          "rds:CreateDBInstance",
+          "rds:CreateDBSnapshot",
+          "rds:CreateDBSubnetGroup",
+          "rds:DeleteDBInstance",
+          "rds:DeleteDBSubnetGroup",
+          "rds:DescribeDBEngineVersions",
+          "rds:DescribeDBInstances",
+          "rds:DescribeDBSubnetGroups",
+          "rds:ListTagsForResource",
+          "rds:CreateDBProxy",
+          "rds:DeleteDBProxy",
+          "rds:DeregisterDBProxyTargets",
+          "rds:DescribeDBProxies",
+          "rds:DescribeDBProxyTargetGroups",
+          "rds:DescribeDBProxyTargets",
+          "rds:ModifyDBInstance",
+          "rds:ModifyDBProxy",
+          "rds:RegisterDBProxyTargets",
+          "rds:RemoveTagsFromResource",
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "SecretsManager"
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:CreateSecret",
+          "secretsmanager:DeleteSecret",
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:GetResourcePolicy",
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:ListSecrets",
+          "secretsmanager:PutSecretValue",
+          "secretsmanager:RestoreSecret",
+          "secretsmanager:RotateSecret",
+          "secretsmanager:TagResource",
+          "secretsmanager:UntagResource",
+          "secretsmanager:UpdateSecret",
+        ]
+        Resource = "arn:aws:secretsmanager:*:*:secret:cudly-*"
+      },
+      {
+        Sid      = "SecretsManagerDescribe"
+        Effect   = "Allow"
+        Action   = ["secretsmanager:ListSecrets"]
+        Resource = "*"
+      },
+      {
+        Sid    = "S3TerraformState"
+        Effect = "Allow"
+        Action = [
+          "s3:DeleteObject",
+          "s3:GetBucketLocation",
+          "s3:GetBucketVersioning",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject",
+        ]
+        Resource = [
+          "arn:aws:s3:::cudly-terraform-state*",
+          "arn:aws:s3:::cudly-terraform-state*/*",
+        ]
+      },
+      {
+        Sid      = "CloudTrailReadOnly"
+        Effect   = "Allow"
+        Action   = ["cloudtrail:LookupEvents"]
+        Resource = "*"
+      },
+    ]
+  })
+
+  tags = {
+    Project   = "CUDly"
+    ManagedBy = "terraform"
+  }
+}
