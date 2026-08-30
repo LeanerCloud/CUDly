6541092f174ca6aa2f6e2ec6c034f5c8772a115b
Cristian Magherusan-Stanciu <cristi.magherusan@gmail.com>
2026-08-30 04:39:48 +0200

    test(common): relax audit helper timeout

--- pkg/common/audit_interprocess_lock_test.go
+++ pkg/common/audit_interprocess_lock_test.go
@@ -61,7 +61,7 @@ func TestWriteAuditRecordWaitsForTransactionLockBeforeRepairingPartialRecord(t *
 	require.NoError(t, holder.Sync())
 
 	readyPath := filepath.Join(t.TempDir(), "helper-ready")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	cmd := exec.CommandContext(ctx, os.Args[0], "-test.run", "^TestAuditLockHelperProcess$")
 	cmd.Env = append(os.Environ(),
